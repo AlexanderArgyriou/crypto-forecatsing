a4a10ec17fe49581c8daeb4bfdb5ce581f122858
Alexandros Argyriou <AArgyriou@pi-ag.com>
2022-04-12 14:47:43 +0300

    front-end improvements

--- crypto-forecast-war/src/main/ui/front-end/src/App.js
+++ crypto-forecast-war/src/main/ui/front-end/src/App.js
@@ -9,19 +9,22 @@ const App = () => {
     <div className="container-fluid">
       <div className='nav-bar-div rel-div'>
         <img className="img-header-left" src={require('./images/mine.png')} />
-        <img className="img-header-left" src={require('./images/mine.png')} />
-        <img className="img-header-left" src={require('./images/mine.png')} />
+        {/* <img className="img-header-left" src={require('./images/mine.png')} />
+        <img className="img-header-left" src={require('./images/mine.png')} /> */}
         <p className="title-header">DSS TimeSeries Forecasting</p>
         <img className="img-header-right" src={require('./images/mine.png')} />
-        <img className="img-header-right" src={require('./images/mine.png')} />
-        <img className="img-header-right" src={require('./images/mine.png')} />
+        {/* <img className="img-header-right" src={require('./images/mine.png')} />
+        <img className="img-header-right" src={require('./images/mine.png')} /> */}
       </div>
       <Predict coin="btc" name={require('./images/btc.png')} />
       <Predict coin="eth" name={require('./images/eth.png')} />
       <Predict coin="sol" name={require('./images/sol.png')} />
       <hr />
       <div className="footer-div">
-        <p>(c) Alexandros Argyriou, all rights reserved.</p>
+        <img className="footer-img-left" src={require('./images/bit-cloud.png')} />
+        <p className="small-p">(c) Alexandros Argyriou, all rights reserved.
+          A master thesis approach on crypto time-series forecatsing.</p>
+          <img className="footer-img-right" src={require('./images/bit-cloud.png')} />
       </div>
     </div>
   )
